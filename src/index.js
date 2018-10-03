d33a604e75aaad81863a3a2e09520af243f8d65a
Samuel Otterman <sjotterman@gmail.com>
2018-10-02 21:13:30 -0400

    Create Header

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
+import 'babel-polyfill'; // Just use all the polyfills. Might not always be needed.
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
+import './styles/styles.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-console.log("hi");
+render(
+  <Router history={browserHistory} routes={routes} />,
+  document.getElementById('app')
+);
